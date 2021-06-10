66bb56cb8a27fdcc5657f303bc6b1f102059d0b2
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-10 12:32:21 +0530

    Initialize amount of bet

---
+++ GamblerProject/GamblerProject/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerProject
+{
+    class StartGame
+    {
+        public void Play()
+        {
+            const int AMOUNT = 1;
+            Console.WriteLine("bet is of "+AMOUNT+" rs");
+        }
+    }
+}
